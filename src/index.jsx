9424d6adc768a6fc36bc89d066b57855c81c062a
Kevin Ulrich <kzulrich@gmail.com>
2022-09-27 17:19:15 -0700

    move to functional component

--- src/index.jsx
+++ src/index.jsx
@@ -1,76 +1,39 @@
 import _Clustergrammer from 'clustergrammer';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-// const Clustergrammer = ({
-//   width = 800, height = 500, id, ref, ...props
-// }) => {
-//   const [mounted, setMounted] = useState(false)
+const Clustergrammer = ({
+  width = 800, height = 500, id, ref, ...props
+}) => {
+  const [mounted, setMounted] = useState(false)
 
-//   const draw = () => {
-//     _Clustergrammer({
-//       ...props,
-//       root: `#react-clustergrammer-box-${this.props.id}`,
-//     });
-//   }
-
-//   useEffect(() => {
-//     if (!mounted) {
-//       setMounted(true)
-//       draw()
-//     }
-//   }, [])
-
-//   return (
-//     <div
-//       id={`react-clustergrammer-box-${this.props.id}`}
-//       style={{ width: `${width}px`, height: `${height}px` }}
-//     />
-//   )
-// }
-
-// Clustergrammer.propTypes = {
-//   width: PropTypes.number,
-//   height: PropTypes.number,
-//   id: PropTypes.string,
-//   ref: PropTypes.any
-// }
-
-// export default Clustergrammer
-
-export default class Clustergrammer extends Component {
-  static propTypes = {
-    width: PropTypes.number,
-    height: PropTypes.number,
-    id: PropTypes.string,
-    ref: PropTypes.any
-  };
-
-  static defaultProps = {
-    width: 800,
-    height: 500,
-  };
-
-  draw = () => {
-    const { width, height, ...clustergrammerProps } = this.props;
+  const draw = () => {
     _Clustergrammer({
-      ...clustergrammerProps,
+      ...props,
       root: `#react-clustergrammer-box-${this.props.id}`,
     });
   }
 
-  componentDidMount() {
-    this.draw();
-  }
+  useEffect(() => {
+    if (!mounted) {
+      setMounted(true)
+      draw()
+    }
+  }, [])
 
-  render() {
-    const { width, height, id, ref } = this.props
-
-    return (
-      <div
-        id={`react-clustergrammer-box-${this.props.id}`}
-        style={{ width: `${width}px`, height: `${height}px` }}
-      />
-    )
-  }
+  return (
+    <div
+      id={`react-clustergrammer-box-${this.props.id}`}
+      style={{ width: `${width}px`, height: `${height}px` }}
+    />
+  )
 }
+
+Clustergrammer.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  id: PropTypes.string,
+  ref: PropTypes.any
+}
+
+export default Clustergrammer
